740d611720f426f7db49c7a0b4a0b03c49532278
ruxi zhang <ruxizhang@ruxis-Mac-Pro.local>
2023-12-07 15:45:49 -0800

    update

--- strings/49_grouped_anagrams.py
+++ strings/49_grouped_anagrams.py
@@ -5,9 +5,30 @@ class Solution(object):
         """
         :type strs: List[str]
         :rtype: List[List[str]]
+        m * nlogn
         """
         res = collections.defaultdict(list)
         for string in strs:
             sorted_str = ''.join(sorted(string))
             res[sorted_str].append(string)
+        return res.values()
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        hashmap
+        O( m * n * 26 )
+        """
+        '''mapping charCount tp list of Anagram'''
+        res = {}
+
+        for s in strs:
+            count = [0] * 26
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            res[tuple(count)].append(s)
+
         return res.values()
