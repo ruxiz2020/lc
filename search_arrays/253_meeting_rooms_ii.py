a0473b515b61bf746330e0b0a96a628d03ca5008
ruxiz2020 <ruxiz2018@gmail.com>
2024-10-25 06:50:46 -0700

    wip

--- search_arrays/253_meeting_rooms_ii.py
+++ search_arrays/253_meeting_rooms_ii.py
@@ -42,6 +42,24 @@ class Solution:
         return res
 
 
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        time = []
+        for i in intervals:
+            start, end = i.start, i.end
+            time.append((start, 1))
+            time.append((end, -1))
+
+        time.sort(key=lambda x: (x[0], x[1]))
+
+        count = 0
+        max_count = 0
+        for t in time:
+            count += t[1]
+            max_count = max(max_count, count)
+        return max_count
+
+
 if __name__ == '__main__':
 
     intervals = [(0,30),(5,10),(15,20)]
